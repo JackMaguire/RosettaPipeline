c2dbfee90eb25b0722dfcd063920755292d9e628
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-10 16:51:21 -0400

    lotta getters and setter

--- src/graph/Node.hh
+++ src/graph/Node.hh
@@ -8,6 +8,91 @@
 namespace graph {
 
 class Node {
+public:
+  Node( int x, int y );
+  Node( String title, int x, int y );
+  ~Node();
+
+public://getters and setters:
+  int ID() const { return id_; }
+  void setID( int id ){ id_ = id; }
+
+  int X() const { return x_; }
+  void setX( int x ){ x_ = x; }
+
+  int Y() const { return y_; }
+  void setY( int y ){ y_ = y; }
+
+  int Red() const { return red_; }
+  void setRed( int r ){ red_ = r; }
+
+  int Green() const { return green_; }
+  void setGreen( int g ){ green_ = g; }
+
+  int Blue() const { return blue_; }
+  void setBlue( int b ){ blue_ = b; }
+  
+  bool useDefaultCommad() const { return use_default_command_; }
+  void setUseDefaultCommand( bool setting ){ use_default_command_ = setting; }
+
+  std::string const & command() const { return command_; }
+  void setCommand( std::string command ){ command_ = std::move( command ); }
+
+  std::string const & title() const { return title_; }
+  void setTitle( std::string title ){ title_ = std::move( title ); }  
+
+  bool useScriptFile() const { return use_script_file_; }
+  void setUseScriptFile( bool setting ){ use_script_file_ = setting; }
+
+  std::string const & xmlScriptFilename() const { return xml_script_filename_; }
+  void setXMLScriptFilename( std::string setting ){ xml_script_filename_ = std::move( setting ); }
+
+  std::string const & xmlScript() const { return xml_script_; }
+  void setXMLScript( std::string setting ){ xml_script_ = std::move( setting ); }  
+
+  int stage() const { return stage_; }//TODO check validity
+  void setStage( int setting ){ stage_ = setting; }
+  void setStageValidity( bool setting ){ stage_is_valid_ = setting; }
+
+public://general compilation access
+  std::string const & getEffectiveCommand() const;
+
+  int inDegreeIgnoringTheseNodes( std::vector< NodeSP > const & nodes_to_ignore ) const;
+
+  std::string dirname() const;
+
+public://edge access and modification
+  int numUpstreamEdges() const;
+
+  EdgeSP getUpstreamEdge( int index );
+
+  EdgeCSP getUpstreamEdge( int index ) const;
+
+  void addUpstreamEdge( EdgeSP const & edge );
+
+  void removeUpstreamEdge( EdgeSP const & edge );
+  
+  int numDownstreamEdges() const;
+
+  EdgeSP getDownstreamEdge( int index );
+
+  EdgeCSP getDownstreamEdge( int index ) const;
+
+  void addDownstreamEdge( EdgeSP const & edge );
+
+  void removeDownstreamEdge( EdgeSP const & edge );
+
+public://container access
+  std::vector< EdgeSP > const & getUpstreamEdges() const { return upstream_edges_; }
+  std::vector< EdgeSP > const & getDownstreamEdges() const { return downstream_edges_; }
+
+  std::vector< std::string > const & userRosettaFlags() const { return user_rosetta_flags_; }
+  void setUserRosettaFlags( std::vector< std::string > new_flags ){
+    user_rosetta_flags_ = std::move( new_flags );
+  }
+
+protected:
+  void init();
 
 private:
   int id_;
@@ -40,5 +125,84 @@ private:
   std::vector< std::string > user_rosetta_flags_;
   std::string notes_;
 };
+
+inline
+std::string const &
+Node::getEffectiveCommand() const {
+  if( use_default_command_ ) {
+    return command_;
+    //TODO
+    //return Options.getDefaultRunCommand();
+  } else {
+    return command_;
+  }
+}
+
+
+inline
+int Node::numUpstreamEdges() const {
+  return upstream_edges_.size();
+};
+
+inline
+EdgeSP Node::getUpstreamEdge( int index ) {
+#ifndef NDEBUG
+  //TODO bounds checking?
+  //std::vector does debug bounds checking anyways so we might not bother
+#endif
+  return upstream_edges_[ index ];
+}
+
+inline
+EdgeCSP Node::getUpstreamEdge( int index ) const {
+  return upstream_edges_[ index ];
+}
+
+inline
+void Node::addUpstreamEdge( EdgeSP const & edge ){
+  upstream_edges_.push_back( edge );
+}
+
+inline
+void Node::removeUpstreamEdge( EdgeSP const & edge ) {
+  auto const iter = std::find( upstream_edges_.begin(), upstream_edges_.end(), edge );
+  if( iter != upstream_edges_.end() ){
+    upstream_edges_.erase( iter );
+  }
+}
+
+
+inline
+int Node::numDownstreamEdges() const {
+  return downstream_edges_.size();
+};
+
+inline
+EdgeSP Node::getDownstreamEdge( int index ) {
+#ifndef NDEBUG
+  //TODO bounds checking?
+  //std::vector does debug bounds checking anyways so we might not bother
+#endif
+  return downstream_edges_[ index ];
+}
+
+inline
+EdgeCSP Node::getDownstreamEdge( int index ) const {
+  return downstream_edges_[ index ];
+}
+
+inline
+void Node::addDownstreamEdge( EdgeSP const & edge ){
+  downstream_edges_.push_back( edge );
+}
+
+inline
+void Node::removeDownstreamEdge( EdgeSP const & edge ) {
+  auto const iter = std::find( downstream_edges_.begin(), downstream_edges_.end(), edge );
+  if( iter != downstream_edges_.end() ){
+    downstream_edges_.erase( iter );
+  }
+}
+
 
 }//namespace graph
