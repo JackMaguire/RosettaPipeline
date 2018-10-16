cd7d171dd0267297c03a6906f6541bbdac46e4ea
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-16 14:48:57 -0400

    Added load

--- src/global_data/options.cc
+++ src/global_data/options.cc
@@ -6,65 +6,84 @@ namespace options {
 // Save/Load
 void GlobalOptions::save( std::vector< std::string > & output_lines ) {
   output_lines.emplace_back( "START_OPTIONS" );
-  output_lines.emplace_back( "show_node_titles " + std::to_string( show_node_titles_ ) );
-  output_lines.emplace_back( "put_node_titles_to_side " + std::to_string( put_node_titles_to_side_ ) );
-  output_lines.emplace_back( "num_processors " + std::to_string( num_processors_ ) );
-  output_lines.emplace_back( "default_run_command " + std::to_string( default_run_command_ ) );
-  output_lines.emplace_back( "delete_unused_intermediate_poses " + std::to_string( delete_unused_intermediate_poses_ ) );
-  output_lines.emplace_back( "grid_size " + std::to_string( grid_size_ ) );
-  output_lines.emplace_back( "serialize_intermediate_poses " + std::to_string( serialize_intermediate_poses_ ) );
+
+  output_lines.emplace_back( "show_node_titles " + std::to_string( show_node_titles ) );
+  output_lines.emplace_back( "put_node_titles_to_side " + std::to_string( put_node_titles_to_side ) );
+  output_lines.emplace_back( "grid_size " + std::to_string( grid_size ) );
+
+  output_lines.emplace_back( "serialize_intermediate_poses " + std::to_string( serialize_intermediate_poses ) );
+  output_lines.emplace_back( "num_processors " + std::to_string( num_processors ) );
+  output_lines.emplace_back( "default_run_command " + default_run_command );
+  output_lines.emplace_back( "delete_unused_intermediate_poses " + std::to_string( delete_unused_intermediate_poses ) );
   output_lines.emplace_back( "END_OPTIONS" );
 }
 
-void GlobalOptions::load( std::vector< std::string > & output_lines ) {
-  final String first_line = in.readLine();
-  if( !first_line.equals( "START_OPTIONS" ) ) {
-    throw new LoadFailureException( "Expected 'START_OPTIONS' instead of '" + first_line + "'" );
-  }
+void GlobalOptions::load( std::vector< std::string > const & lines, int line_to_start_at ) {
 
-  for( String line = in.readLine(); !line.equals( "END_OPTIONS" ); line = in.readLine() ) {
-    final String[] split = line.split( "\\s+" );
-    if( split.length == 0 )
-      continue;
+  unsigned int current_line = line_to_start_at;
 
-    if( split[ 0 ].equals( "show_node_titles" ) ) {
-      show_node_titles_ = Bool.parseBool( split[ 1 ] );
+  assert( lines[ current_line ] == "START_NODE" );
+
+  while( lines[ ++current_line ] != "END_NODE" ){
+    std::string const line = lines[ current_line ];
+
+    std::vector< std::string > tokens;
+    {//stolen from https://stackoverflow.com/questions/13172158/c-split-string-by-line
+      std::string const delimiter = " ";
+      std::string::size_type prev = 0;
+      std::string::size_type pos = line.find( delimiter, prev );
+      while ( pos != std::string::npos ) {
+	tokens.push_back( line.substr( prev, pos - prev ) );
+	prev = pos + 1;
+	pos = line.find( delimiter, prev );
+      }
+
+      // To get the last substring (or only, if delimiter is not found)
+      tokens.push_back( line.substr( prev ) );
+    }
+
+    if( tokens.size() < 2 ) continue;
+
+    if( tokens[ 0 ] == "show_node_titles" ) {
+      show_node_titles = ( tokens[ 1 ] == "1" );
       continue;
     }
 
-    if( split[ 0 ].equals( "put_node_titles_to_side" ) ) {
-      put_node_titles_to_side_ = Bool.parseBool( split[ 1 ] );
+    if( tokens[ 0 ] == "put_node_titles_to_side" ) {
+      put_node_titles_to_side = ( tokens[ 1 ] == "1" );
       continue;
     }
 
-    if( split[ 0 ].equals( "num_processors" ) ) {
-      num_processors_ = Integer.parseInt( split[ 1 ] );
+    if( tokens[ 0 ] == "grid_size" ) {
+      num_processors = std::stoi( tokens[ 1 ] );
       continue;
     }
 
-    if( split[ 0 ].equals( "grid_size" ) ) {
-      grid_size_ = Integer.parseInt( split[ 1 ] );
+
+    if( tokens[ 0 ] == "serialize_intermediate_poses" ) {
+      serialize_intermediate_poses = ( tokens[ 1 ] == "1" );
       continue;
     }
 
-    if( split[ 0 ].equals( "default_run_command" ) ) {
-      default_run_command_ = split[ 1 ];
-      for( int i = 2; i < split.length; ++i ) {
-	default_run_command_ += " " + split[ i ];
+    if( tokens[ 0 ] == "num_processors" ) {
+      num_processors = std::stoi( tokens[ 1 ] );
+      continue;
+    }
+
+    if( tokens[ 0 ] == "title" ) {
+      default_run_command = tokens[ 1 ];
+      for( unsigned int i = 2; i < tokens.size(); ++i ) {
+	default_run_command += " " + tokens[ i ];
       }
       continue;
     }
 
-    if( split[ 0 ].equals( "delete_unused_intermediate_poses" ) ) {
-      delete_unused_intermediate_poses_ = Bool.parseBool( split[ 1 ] );
+    if( tokens[ 0 ] == "delete_unused_intermediate_poses" ) {
+      delete_unused_intermediate_poses = ( tokens[ 1 ] == "1" );
       continue;
     }
 
-    if( split[ 0 ].equals( "serialize_intermediate_poses" ) ) {
-      serialize_intermediate_poses_ = Bool.parseBool( split[ 1 ] );
-      continue;
-    }
-  }
+  }//while not "END_NODE"
 }
 
 
