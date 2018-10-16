407eab23951186fa972b57a5c5da2acdfbc541a4
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-16 11:52:13 -0400

    new pilot app

---
+++ src/apps/proof_of_concept/testing_ground.cc
@@ -0,0 +1,26 @@
+#include <Wt/WContainerWidget>
+#include <Wt/WHBoxLayout>
+#include <Wt/WText>
+
+#include <devel/DummyPanel.hh>
+
+#include <memory>
+
+using namespace Wt;
+
+int main(){
+  
+  auto container = std::make_shared< Wt::WContainerWidget >();
+
+  std::unique_ptr< Wt::WLayout > hbox = std::make_unique< Wt::WHBoxLayout >();
+  //Wt::WHBoxLayout *hbox = new Wt::WHBoxLayout();//demo uses raw pointers
+  container->setLayout( hbox );
+
+  std::unique_ptr< devel::MyPaintedWidget > left_side = std::make_unique< devel::MyPaintedWidget >();
+  hbox->addWidget( left_side );
+
+  std::unique_ptr< devel::MyPaintedWidget > right_side = std::make_unique< devel::MyPaintedWidget >();
+  hbox->addWidget( right_side );
+
+  return 0;
+}
