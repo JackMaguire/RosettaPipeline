5c609e6f87422085f52c811e7f133153a754254d
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-18 16:28:31 -0400

    drawing background and lines

--- src/view/graph_view.cc
+++ src/view/graph_view.cc
@@ -12,8 +12,8 @@ GraphWidget::GraphWidget() :
   width_( 200 ),
   height_( 20 )
 {
-  width_ = 200;
-  height_ = 20;
+  width_ = width();
+  height_ = height();
 
   setLayoutSizeAware( true );
 
@@ -31,10 +31,22 @@ GraphWidget::layoutSizeChanged( int w, int h ) override {
 void
 GraphWidget::paintEvent( Wt::WPaintDevice * paintDevice ) {
   Wt::WPainter painter(paintDevice);
+
+  //Background
+  //
   painter.setBrush( Wt::WBrush( theme_->background() ) );
   painter.drawRect( 0, 0, width_, height_ );
 
+  //Grid
+  //
+  painter.setBrush( Wt::WBrush( theme_->grid_line() ) );
   int const grid_size = global_data::Options::grid_size;
+  for( int x = -1; x < width_; x += 2 * grid_size ) {
+    painter.drawLine( x, 0, x, height_ );
+  }
+  for( int y = -1; y < height_; y += 2 * grid_size ) {
+    painter.drawLine( 0, y, width_, y );
+  }
 }
 
 
