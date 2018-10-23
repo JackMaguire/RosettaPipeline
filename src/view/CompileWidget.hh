a3b803e4e45db14a785d90b95f2084024ccc5556
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-23 19:09:57 -0400

    Added dummy widgets

---
+++ src/view/CompileWidget.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Wt/WTabWidget>
+#include <graph/Graph.fwd.hh>
+
+namespace view {
+
+class CompileWidget : public Wt::WTabWidget
+{
+public:
+  CompileWidget( Wt::WContainerWidget *root, graph::GraphSP const & );
+
+  virtual ~CompileWidget();
+
+  void layoutSizeChanged( int w, int h ) override;
+
+private:
+  int width_;
+  int height_;
+};
+
+inline
+void
+CompileWidget::layoutSizeChanged( int w, int h ) {
+  width_ = w;
+  height_ = h;
+}
+
+
+}//namespace view
---
+++ src/view/EditWidget.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Wt/WTabWidget>
+#include <graph/Graph.fwd.hh>
+
+namespace view {
+
+class EditWidget : public Wt::WTabWidget
+{
+public:
+  EditWidget( Wt::WContainerWidget *root, graph::GraphSP const & );
+
+  virtual ~EditWidget();
+
+  void layoutSizeChanged( int w, int h ) override;
+
+private:
+  int width_;
+  int height_;
+};
+
+inline
+void
+EditWidget::layoutSizeChanged( int w, int h ) {
+  width_ = w;
+  height_ = h;
+}
+
+
+}//namespace view
