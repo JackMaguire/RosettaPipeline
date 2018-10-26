515008f4e1a0b111cf524563ed4707160f8ad15a
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-26 14:51:18 -0400

    added interactive functionality

--- src/view/LoadWidget.cc
+++ src/view/LoadWidget.cc
@@ -1,13 +1,16 @@
 #include <view/LoadWidget.hh>
-#include <global_data/options.hh>
 
+#include <global_data/options.hh>
 #include <graph/Graph.fwd.hh>
 
-#include <Wt/WLength.h>
+//#include <Wt/WLength.h>
 #include <Wt/WGlobal.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WText.h>
 
 #include <iostream>
 #include <memory>
+#include <string>
 
 namespace view {
 
@@ -21,8 +24,33 @@ LoadWidget::LoadWidget(
   setLayoutSizeAware( true );
   resize( width_, height_ ); // Provide a default size.
 
-  using namespace Wt;
+  //using namespace Wt;
 
+  Wt::WPushButton * uploadButton = addWidget( Wt::cpp14::make_unique< Wt::WPushButton >( "Load" ) );
+  Wt::WText * out = container->addWidget( Wt::cpp14::make_unique< Wt::WText >() );
+
+  uploadButton->clicked().connect(
+    [=] {
+      upload();
+      uploadButton->disable();
+      out->setText("File upload is finished.");
+    }
+  );
+
+  uploaded().connect(
+    [=] {
+      uploadButton->enable();
+      out->setText( "File upload is finished. Total num files uploaded: " +
+	std::to_string( uploadedFiles().size() ) );
+    }
+  );
+
+  fileTooLarge().connect(
+    [=] {
+      uploadButton->enable();
+      out->setText("File is too large.");
+    }
+  );
 }
 
 LoadWidget::~LoadWidget(){}
