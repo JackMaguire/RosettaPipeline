ffb2701f3794fea17447df2ff5c33377d97f9868
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-28 13:10:06 -0400

    fixing files to match class names (3)

--- src/apps/proof_of_concept/graph_view_app.cc
+++ src/apps/proof_of_concept/graph_view_app.cc
@@ -1,19 +1,18 @@
 #include <Wt/WApplication.h>
 
 #include <graph/Graph.hh>
-#include <view/graph_view.hh>
-#include <view/top_level_view.hh>
+//#include <view/GraphWidget.hh>
+#include <view/TopWidget.hh>
 
 #include <memory>
 
 using namespace Wt;
 
-std::unique_ptr<WApplication> createApplication(const WEnvironment& env)
-{
+std::unique_ptr< Wt::WApplication > createApplication( Wt::WEnvironment const & env ) {
   graph::GraphSP graph = std::make_shared< graph::Graph >();
   graph::NodeSP previous = 0;
   for( int i=0; i < 3; ++i ){
-    graph::NodeSP node = std::make_shared< graph::Node >( i*10, i*5);
+    graph::NodeSP node = std::make_shared< graph::Node >( 20, i*15);
     graph->addNode( node );
     if( i > 0 ){
       graph->addEdge( previous, node );
@@ -21,15 +20,15 @@ std::unique_ptr<WApplication> createApplication(const WEnvironment& env)
     previous = node;
   }
 
-  std::unique_ptr<WApplication> app = Wt::cpp14::make_unique<WApplication>(env);
-  app->setTitle("Rosetta Pipeline");
-  app->root()->setStyleClass("root");
-  app->root()->addWidget( cpp14::make_unique< view::TopWidget >( graph ) );
+  std::unique_ptr< Wt::WApplication > app = Wt::cpp14::make_unique< Wt::WApplication >( env );
+  app->setTitle( "Rosetta Pipeline" );
+  app->root()->setStyleClass( "root" );
+  app->root()->addWidget( Wt::cpp14::make_unique< view::TopWidget >( graph ) );
   //app->useStyleSheet("dragdrop.css");
   return app;
 }
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
-  return WRun(argc, argv, &createApplication);
+  return WRun( argc, argv, &createApplication );
 }
