170b455c11c2e223cf4fc14680cf03ffffe19b6f
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-29 17:41:59 -0400

    testing spin box

--- src/view/OptionsWidget.cc
+++ src/view/OptionsWidget.cc
@@ -7,6 +7,7 @@
 #include <Wt/WPushButton.h>
 #include <Wt/WBreak.h>
 #include <Wt/WCheckBox.h>
+#include <Wt/WSpinBox.h>
 
 #include <iostream>
 #include <memory>
@@ -97,19 +98,62 @@ OptionsWidget::setup_run_options(){
   addWidget( Wt::cpp14::make_unique< Wt::WText >( "Run Options:" ) );
   addWidget( Wt::cpp14::make_unique< Wt::WBreak >() );
 
-  Wt::WCheckBox * serialize_intermediate_poses_box =
-    addWidget( Wt::cpp14::make_unique< Wt::WCheckBox >( "<i>Serialize Intermediate Poses.</i>" ) );
-  addWidget( Wt::cpp14::make_unique< Wt::WText >( " This option can speed up runs by saving poses in a faster-to-read format. This format cannot be read by any software other than Rosetta, so we will not apply this option to terminal stages (stages with no nodes after). Requires Rosetta to be built with extras=serialization." ) );
-  addWidget( Wt::cpp14::make_unique< Wt::WBreak >() );
-
   Wt::WCheckBox * delete_unused_intermediate_poses_box =
     addWidget( Wt::cpp14::make_unique< Wt::WCheckBox >( "<b>Delete Unused Intermediate Poses.</b>" ) );
   addWidget( Wt::cpp14::make_unique< Wt::WText >( " This option can save disk space by deleting poses that are no longer needed. We will not delete any poses that result from terminal stages becase we assume you might want to look at those. We will also not delete any input structures." ) );
   addWidget( Wt::cpp14::make_unique< Wt::WBreak >() );
 
-  serialize_intermediate_poses_box->setTextFormat( Wt::TextFormat::XHTML );
-  delete_unused_intermediate_poses_box->setTextFormat( Wt::TextFormat::XHTML );
+  Wt::WCheckBox * serialize_intermediate_poses_box =
+    addWidget( Wt::cpp14::make_unique< Wt::WCheckBox >( "<b>Serialize Intermediate Poses.</b>" ) );
+  addWidget( Wt::cpp14::make_unique< Wt::WText >( " This option can speed up runs by saving poses in a faster-to-read format. This format cannot be read by any software other than Rosetta, so we will not apply this option to terminal stages (stages with no nodes after). Requires Rosetta to be built with extras=serialization." ) );
+  addWidget( Wt::cpp14::make_unique< Wt::WBreak >() );
 
+  Wt::WSpinBox * num_proc_spin_box =
+    addWidget( Wt::cpp14::make_unique< Wt::WCheckBox >( "Number Of Processors" ) );
+  addWidget( Wt::cpp14::make_unique< Wt::WText >( " This value is saved as $nproc in the run.sh script" ) );
+  addWidget( Wt::cpp14::make_unique< Wt::WBreak >() );
+
+
+  //delete_unused_intermediate_poses_box
+  delete_unused_intermediate_poses_box->setChecked( Options::delete_unused_intermediate_poses );
+  delete_unused_intermediate_poses_box->setTextFormat( Wt::TextFormat::XHTML );
+  delete_unused_intermediate_poses_box->checked().connect (
+    [=] {
+      Options::delete_unused_intermediate_poses = true;
+    }
+  );
+  delete_unused_intermediate_poses_box->unChecked().connect (
+    [=] {
+      Options::delete_unused_intermediate_poses = false;
+    }
+  );
+
+
+  //serialize_intermediate_poses_box
+  serialize_intermediate_poses_box->setChecked( Options::serialize_intermediate_poses );
+  serialize_intermediate_poses_box->setTextFormat( Wt::TextFormat::XHTML );
+  serialize_intermediate_poses_box->checked().connect (
+    [=] {
+      Options::serialize_intermediate_poses = true;
+    }
+  );
+  serialize_intermediate_poses_box->unChecked().connect (
+    [=] {
+      Options::serialize_intermediate_poses = false;
+    }
+  );
+
+
+  //num_proc_spin_box
+  num_proc_spin_box->setValue( Options::num_processors );
+  num_proc_spin_box->setMinimum( 0 );
+  num_proc_spin_box->changed().connect(
+    [=] {
+      if( num_proc_spin_box->validate() == Wt::ValidationState::Valid ){
+	Options::num_processors = std::stoi();
+      }
+    }
+  );
 }
 
 }//namespace view
