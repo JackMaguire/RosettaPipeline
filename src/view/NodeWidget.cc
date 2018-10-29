fa88281979584d24c1fa8d29c4db97219ae0d9fb
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-29 15:55:08 -0400

    added debug output

--- src/view/NodeWidget.cc
+++ src/view/NodeWidget.cc
@@ -75,6 +75,7 @@ NodeWidget::construct_segment1(
 
   command_edit->keyPressed().connect(
     [=] ( Wt::WKeyEvent const & e ) {
+      std::cout << "PRESSED " << command_edit->text().narrow() << std::endl;
       node_->setCommand( command_edit->text().narrow() );
     }
   );
