215359b2718d9892bd1c5b9f948ce5d23fc663de
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-29 10:18:45 -0400

    Added all elements to compile page

--- src/view/CompileWidget.cc
+++ src/view/CompileWidget.cc
@@ -7,6 +7,7 @@
 #include <Wt/WGlobal.h>
 #include <Wt/WVBoxLayout.h>
 #include <Wt/WBorderLayout.h>
+#include <Wt/WTextArea.h>
 
 #include <iostream>
 #include <memory>
@@ -57,32 +58,56 @@ struct LayoutsAndContainers {
     border_layout =
       root->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
 
+    //A
+    compile_button =
+      border_layout->addWidget( Wt::cpp14::make_unique< Wt::WPushButton >( "Compile" ), Wt::LayoutPosition::North );
+
     center_container =
       border_layout->addWidget( Wt::cpp14::make_unique< Wt::WContainerWidget >(), Wt::LayoutPosition::Center );
 
     center_layout =
       center_container->setLayout( Wt::cpp14::make_unique< Wt::WVBoxLayout >() );
 
+    //top_center == B & C
     top_center_container =
       center_layout->addWidget( Wt::cpp14::make_unique< Wt::WContainerWidget >() );
 
     top_center_layout =
       top_center_container->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
 
+    //top_center == D & E
     bottom_center_container =
       center_layout->addWidget( Wt::cpp14::make_unique< Wt::WContainerWidget >() );
 
     bottom_center_layout =
       bottom_center_container->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+
+    //B
+    top_center_layout->addWidget( Wt::cpp14::make_unique< Wt::WText >( "Setup.sh" ), Wt::LayoutPosition::North );
+
+    //C
+    setup_script_area =
+      top_center_layout->addWidget( Wt::cpp14::make_unique< Wt::WTextArea >(), Wt::LayoutPosition::Center );
+
+    //D
+    bottom_center_layout->addWidget( Wt::cpp14::make_unique< Wt::WText >( "Run.sh" ), Wt::LayoutPosition::North );
+
+    //E
+    run_script_area =
+      bottom_center_layout->addWidget( Wt::cpp14::make_unique< Wt::WTextArea >(), Wt::LayoutPosition::Center );
   }
 
   Wt::WBorderLayout * border_layout;
-  Wt::WContainerWidget * center_container;
-  Wt::WVBoxLayout * center_layout;
-  Wt::WContainerWidget * top_center_container;
-  Wt::WContainerWidget * top_center_layout;
-  Wt::WContainerWidget * bottom_center_container;
-  Wt::WContainerWidget * bottom_center_layout;
+  Wt::WPushButton * compile_button;//A
+  Wt::WContainerWidget * center_container;//BCDE
+  Wt::WVBoxLayout * center_layout;//BCDE
+  Wt::WContainerWidget * top_center_container;//BC
+  Wt::WBorderLayout * top_center_layout;//BC
+  Wt::WContainerWidget * bottom_center_container;//DE
+  Wt::WBorderLayout * bottom_center_layout;//DE
+
+  Wt::WTextArea * setup_script_area;//C
+  Wt::WTextArea * run_script_area;//E
 };
 
 }
@@ -92,9 +117,7 @@ CompileWidget::CompileWidget(
   graph::GraphSP const & graph
 ) :
   WContainerWidget( )
-{
-  Wt::WPushButton * compile_button = addWidget( Wt::cpp14::make_unique< Wt::WPushButton >( "Load" ) );
- 
+{ 
   LayoutsAndContainers elements( this );
 }
 
