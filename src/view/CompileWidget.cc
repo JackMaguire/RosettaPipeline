00db02ad80fc81e4ddc7ed777154260741ca2c28
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-29 09:47:10 -0400

    compile?

--- src/view/CompileWidget.cc
+++ src/view/CompileWidget.cc
@@ -48,12 +48,23 @@ struct LayoutsAndContainers {
     top_center_container =
       center_layout->addWidget( Wt::cpp14::make_unique< Wt::WContainerWidget >() );
 
+    top_center_layout =
+      top_center_container->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+
+    bottom_center_container =
+      center_layout->addWidget( Wt::cpp14::make_unique< Wt::WContainerWidget >() );
+
+    bottom_center_layout =
+      bottom_center_container->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
   }
 
   Wt::WBorderLayout * border_layout;
   Wt::WContainerWidget * center_container;
   Wt::WVBoxLayout * center_layout;
   Wt::WContainerWidget * top_center_container;
+  Wt::WContainerWidget * top_center_layout;
+  Wt::WContainerWidget * bottom_center_container;
+  Wt::WContainerWidget * bottom_center_layout;
 };
 
 }
