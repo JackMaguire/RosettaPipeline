f215d6f8ac4d80f5f3d22fef6dbfa784ec58260b
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-29 19:03:24 -0400

    setWordWrap

--- src/view/OptionsWidget.cc
+++ src/view/OptionsWidget.cc
@@ -103,12 +103,12 @@ OptionsWidget::setup_run_options(){
 
   Wt::WCheckBox * delete_unused_intermediate_poses_box =
     addWidget( Wt::cpp14::make_unique< Wt::WCheckBox >( "<b>Delete Unused Intermediate Poses.</b>" ) );
-  addWidget( Wt::cpp14::make_unique< Wt::WText >( " This option can save disk space by deleting poses that are no longer needed. We will not delete any poses that result from terminal stages becase we assume you might want to look at those. We will also not delete any input structures." ) );
+  addWidget( Wt::cpp14::make_unique< Wt::WText >( " This option can save disk space by deleting poses that are no longer needed. We will not delete any poses that result from terminal stages becase we assume you might want to look at those. We will also not delete any input structures." ) )->setWordWrap( true );
   addWidget( Wt::cpp14::make_unique< Wt::WBreak >() );
 
   Wt::WCheckBox * serialize_intermediate_poses_box =
     addWidget( Wt::cpp14::make_unique< Wt::WCheckBox >( "<b>Serialize Intermediate Poses.</b>" ) );
-  addWidget( Wt::cpp14::make_unique< Wt::WText >( " This option can speed up runs by saving poses in a faster-to-read format. This format cannot be read by any software other than Rosetta, so we will not apply this option to terminal stages (stages with no nodes after). Requires Rosetta to be built with extras=serialization." ) );
+  addWidget( Wt::cpp14::make_unique< Wt::WText >( " This option can speed up runs by saving poses in a faster-to-read format. This format cannot be read by any software other than Rosetta, so we will not apply this option to terminal stages (stages with no nodes after). Requires Rosetta to be built with extras=serialization." ) )->setWordWrap( true );
   addWidget( Wt::cpp14::make_unique< Wt::WBreak >() );
 
   Wt::WSpinBox * num_proc_spin_box =
