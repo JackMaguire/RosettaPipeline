376576d7efb91460bc5d57d3964c114009033cf5
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-29 16:39:57 -0400

    recycling EditWidget into OptionsWidget

---
+++ src/view/OptionsWidget.cc
@@ -0,0 +1,19 @@
+#include <view/OptionsWidget.hh>
+
+#include <global_data/options.hh>
+#include <Wt/WGlobal.h>
+
+#include <iostream>
+#include <memory>
+
+namespace view {
+
+OptionsWidget::OptionsWidget() :
+  WContainerWidget(),
+{
+}
+
+OptionsWidget::~OptionsWidget(){}
+
+
+}//namespace view
---
+++ src/view/OptionsWidget.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Wt/WContainerWidget.h>
+
+namespace view {
+
+class OptionsWidget : public Wt::WContainerWidget
+{
+public:
+  OptionsWidget();
+
+  virtual ~OptionsWidget();
+};
+
+
+
+}//namespace view
