62042408b60163c72b69bd7e0056884650d10487
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-29 11:11:11 -0400

    test 2

--- src/view/EditWidget.cc
+++ src/view/EditWidget.cc
@@ -46,14 +46,14 @@ EditWidget::noteChangeInSelection(){
   auto uniq_ptr = layout_->removeWidget( current_ );
   uniq_ptr = 0;
 
-  /*graph::NodeSP selected_node = graph_->selectedNode();
+  graph::NodeSP selected_node = graph_->selectedNode();
   if( selected_node ){
     current_ = layout_->addWidget( Wt::cpp14::make_unique< NodeWidget >( selected_node ), Wt::LayoutPosition::Center );
   } else {
     graph::EdgeSP selected_edge = graph_->selectedEdge();
     assert( selected_edge );
     current_ = layout_->addWidget( Wt::cpp14::make_unique< EdgeWidget >( selected_edge ), Wt::LayoutPosition::Center );
-  }*/
+  }
 
 }
 
