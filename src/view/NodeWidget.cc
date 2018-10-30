1d3b5a47404cd3dce0895e4882faf4092cf7932f
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-30 16:45:39 -0400

    trying code plugin

--- src/view/NodeWidget.cc
+++ src/view/NodeWidget.cc
@@ -43,6 +43,7 @@ NodeWidget::NodeWidget(
     bottom_layout->addWidget( Wt::cpp14::make_unique< Wt::WTextEdit >( node_->xmlScript() ), Wt::LayoutPosition::Center );
   script_editor->setMinimumSize( 500, 200 );
   //script_editor->setExtraPlugins( "advcode" );
+  script_editor->setExtraPlugins( "code" );
 }
 
 NodeWidget::~NodeWidget(){
