fe7b8d28a4b79607511f560ce798bdb0952c1929
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-30 14:59:13 -0400

    setting minimum sizes

--- src/view/TopWidget.cc
+++ src/view/TopWidget.cc
@@ -30,9 +30,13 @@ TopWidget::TopWidget( graph::GraphSP const & graph ) :
 
   Wt::WContainerWidget * const right_container =
     hbox->addWidget( Wt::cpp14::make_unique< Wt::WContainerWidget >(), 2 );
+  right_container->setMinimumSize( 500, 1000 );
+
   Wt::WBorderLayout * const right_layout =
     right_container->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
   right_layout->setSpacing( 0 );
+  //right_layout->
+
   right_layout->addWidget( Wt::cpp14::make_unique< RightViewWidget >( graph, left_item ), Wt::LayoutPosition::Center );
 
 }
