881014189d83b2159a49d901ebdd0e8628f7ea09
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-01 17:08:37 -0400

    util.hh

---
+++ src/util.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+//Don't look, macros!!!
+#define STR(str) #str
+#define STRING(str) STR(str)
+
+std::string
+getPathToTopDirectory(){
+  std::string path_to_this_file = STRING( __FILE__ );//Users/.../RosettaPipeline/src/util.hh
+  std::string const toErase = "src/util.hh";
+  size_t pos = path_to_this_file.find( toErase );
+  assert( pos != std::string::npos );
+  path_to_this_file.erase( pos, toErase.length() );
+  return path_to_this_file;
+}
