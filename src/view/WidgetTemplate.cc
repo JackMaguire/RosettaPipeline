a0b2061fa828626d6db269fcace3caf9c457d66a
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-02 16:48:28 -0400

    Added Widget Templates

---
+++ src/view/WidgetTemplate.cc
@@ -0,0 +1,26 @@
+#include <view/WidgetTemplate.hh>
+#include <view/RightViewWidget.hh>
+#include <view/LeftViewWidget.hh>
+#include <view/GraphWidget.hh>
+#include <view/GraphToolbarWidget.hh>
+
+//#include <global_data/options.hh>
+
+//#include <Wt/WLength.h>
+//#include <Wt/WImage.h>
+#include <Wt/WGlobal.h>
+//#include <Wt/WHBoxLayout.h>
+//#include <Wt/WVBoxLayout.h>
+//#include <Wt/WBorderLayout.h>
+//#include <Wt/WLink.h>
+
+namespace view {
+
+WidgetTemplate::WidgetTemplate( ) :
+  WContainerWidget()
+{
+}
+
+WidgetTemplate::~WidgetTemplate(){}
+
+}//namespace view
---
+++ src/view/WidgetTemplate.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Wt/WContainerWidget.h>
+//#include <graph/Graph.fwd.hh>
+
+namespace view {
+
+class WidgetTemplate : public Wt::WContainerWidget
+{
+public:
+  WidgetTemplate();
+
+  virtual ~WidgetTemplate();
+
+};
+
+}//namespace view
