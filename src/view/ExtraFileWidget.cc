05bce8283a6c31de809ad5a572d1e3b773a7b039
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-02 17:16:50 -0400

    finished first draft of extra file widget

--- src/view/ExtraFileWidget.cc
+++ src/view/ExtraFileWidget.cc
@@ -1,8 +1,4 @@
 #include <view/ExtraFileWidget.hh>
-#include <view/RightViewWidget.hh>
-#include <view/LeftViewWidget.hh>
-#include <view/GraphWidget.hh>
-#include <view/GraphToolbarWidget.hh>
 
 //#include <global_data/options.hh>
 
@@ -11,14 +7,36 @@
 #include <Wt/WGlobal.h>
 //#include <Wt/WHBoxLayout.h>
 //#include <Wt/WVBoxLayout.h>
-//#include <Wt/WBorderLayout.h>
+#include <Wt/WBorderLayout.h>
 //#include <Wt/WLink.h>
+#include <Wt/WText.h>
 
 namespace view {
 
 ExtraFileWidget::ExtraFileWidget( ) :
   WContainerWidget()
 {
+  Wt::WBorderLayout * const layout =
+    setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+
+  Wt::WContainerWidget * const upper_container =
+    layout->addWidget( Wt::cpp14::make_unique< Wt::WContainerWidget >(),
+      Wt::LayoutPosition::North );
+    
+  Wt::WBorderLayout * const upper_layout =
+    upper_container->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+
+  upper_layout->addWidget( Wt::cpp14::make_unique< Wt::WText >( "Filename:" ),
+    Wt::LayoutPosition::West );
+
+  file_name_ =
+    upper_layout->addWidget( Wt::cpp14::make_unique< Wt::WLineEdit >( "filename" ),
+      Wt::LayoutPosition::Center );
+
+  content_editor_ =
+    layout->addWidget( Wt::cpp14::make_unique< Wt::WTextArea >( ),
+      Wt::LayoutPosition::Center );
+
 }
 
 ExtraFileWidget::~ExtraFileWidget(){}
--- src/view/ExtraFileWidget.hh
+++ src/view/ExtraFileWidget.hh
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <Wt/WContainerWidget.h>
+#include <Wt/WTextArea.h>
+#include <Wt/WLineEdit.h>
 //#include <graph/Graph.fwd.hh>
+
+#include <string>
 
 namespace view {
 
@@ -12,6 +16,33 @@ public:
 
   virtual ~ExtraFileWidget();
 
+  std::string getFileName() const;
+
+  std::string getFileContents() const;
+
+private:
+  Wt::WLineEdit * file_name_;
+  Wt::WTextArea * content_editor_;
 };
+
+inline
+std::string
+ExtraFileWidget::getFileName() const {
+  if( file_name_ ){
+    return file_name_->text().narrow();
+  } else {
+    return "";
+  }
+}
+
+inline
+std::string
+ExtraFileWidget::getFileContents() const {
+  if( content_editor_ ){
+    return content_editor_->text().narrow();
+  } else {
+    return "";
+  }
+}
 
 }//namespace view
