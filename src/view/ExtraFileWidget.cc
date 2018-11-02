342c5c00e1f439ee613e289c119e0a022c597c3d
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-02 16:49:51 -0400

    extra file widgets

---
+++ src/view/ExtraFileWidget.cc
@@ -0,0 +1,26 @@
+#include <view/ExtraFileWidget.hh>
+#include <view/RightViewWidget.hh>
+#include <view/LeftViewWidget.hh>
+#include <view/GraphWidget.hh>
+#include <view/GraphToolbarWidget.hh>
+
+//#include <global_data/options.hh>
+
+//#include <Wt/WLength.h>
+//#include <Wt/WImage.h>
+#include <Wt/WGlobal.h>
+//#include <Wt/WHBoxLayout.h>
+//#include <Wt/WVBoxLayout.h>
+//#include <Wt/WBorderLayout.h>
+//#include <Wt/WLink.h>
+
+namespace view {
+
+ExtraFileWidget::ExtraFileWidget( ) :
+  WContainerWidget()
+{
+}
+
+ExtraFileWidget::~ExtraFileWidget(){}
+
+}//namespace view
---
+++ src/view/ExtraFileWidget.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Wt/WContainerWidget.h>
+//#include <graph/Graph.fwd.hh>
+
+namespace view {
+
+class ExtraFileWidget : public Wt::WContainerWidget
+{
+public:
+  ExtraFileWidget();
+
+  virtual ~ExtraFileWidget();
+
+};
+
+}//namespace view
