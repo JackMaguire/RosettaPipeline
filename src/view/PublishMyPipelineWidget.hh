8d149f0de26cae58430bed80c9cb704e0cd57441
Jack Maguire <jack@med.unc.edu>
2018-11-04 09:37:12 -0500

    PublishMyPipelineWidget

---
+++ src/view/PublishMyPipelineWidget.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Wt/WContainerWidget.h>
+//#include <graph/Graph.fwd.hh>
+
+namespace view {
+
+class PublishMyPipelineWidget : public Wt::WContainerWidget
+{
+public:
+  PublishMyPipelineWidget();
+
+  virtual ~PublishMyPipelineWidget();
+
+};
+
+}//namespace view
