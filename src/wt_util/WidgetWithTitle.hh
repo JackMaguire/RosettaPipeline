b2fbc3cdbc4654273d47df9640836c67456e8a68
Jack Maguire <jack@med.unc.edu>
2018-11-04 09:47:23 -0500

    Made helpful template

--- src/wt_util/WidgetWithTitle.hh
+++ src/wt_util/WidgetWithTitle.hh
@@ -1,28 +1,28 @@
 #pragma once
 
 #include <Wt/WContainerWidget.h>
-//#include <graph/Graph.fwd.hh>
-
-//#include <Wt/WLength.h>
-//#include <Wt/WImage.h>
 #include <Wt/WGlobal.h>
-//#include <Wt/WHBoxLayout.h>
-//#include <Wt/WVBoxLayout.h>
-//#include <Wt/WBorderLayout.h>
-//#include <Wt/WLink.h>
+#include <Wt/WBorderLayout.h>
+#include <Wt/WText.h>
 
 
 namespace wt_util {
-
 
 template< typename T, typename... Args >
 class WidgetWithTitle : public Wt::WContainerWidget
 {
 public:
-  WidgetWithTitle() :
+  WidgetWithTitle( std::string const & title, Args&&... args ) :
     WContainerWidget()
   {
-    
+    Wt::WBorderLayout * const layout =
+      setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+    layout->addWidget( Wt::cpp14::make_unique< Wt::WText >( title ),
+      Wt::LayoutPosition::West );
+
+    subwidget_ = 
+      layout->addWidget( Wt::cpp14::make_unique< T >( std::forward< Args >( args )... ),
+	Wt::LayoutPosition::Center );
   }
 
   virtual ~WidgetWithTitle(){}
