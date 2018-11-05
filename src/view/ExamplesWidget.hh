13e710ae3691fda6f4fcfc411c5fa806fbc135d7
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-05 18:10:22 -0500

    No Global Data Initiative: ExamplesWidget.hh

--- src/view/ExamplesWidget.hh
+++ src/view/ExamplesWidget.hh
@@ -3,11 +3,13 @@
 #include <Wt/WContainerWidget.h>
 #include <graph/Graph.fwd.hh>
 #include <view/GraphWidget.fwd.hh>
-#include <view/RightViewWidget.hh>
+//#include <view/RightViewWidget.hh>
 
 #include <Options.hh>
 
 namespace view {
+
+class RightViewWidget;
 
 class ExamplesWidget : public Wt::WContainerWidget, OptionsHolder
 {
