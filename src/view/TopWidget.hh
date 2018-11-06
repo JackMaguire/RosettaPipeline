7546397aa0f38954ccec2a41e9d9e1c1fe16f1c4
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-06 10:02:32 -0500

    added debug info (5)

--- src/view/TopWidget.hh
+++ src/view/TopWidget.hh
@@ -3,6 +3,7 @@
 #include <Wt/WContainerWidget.h>
 #include <graph/Graph.fwd.hh>
 #include <Options.hh>
+#include <iostream>
 
 namespace view {
 
@@ -18,6 +19,8 @@ public:
 
   void layoutSizeChanged( int w, int h ) override;
 
+  void parentResized( Wt::WWidget *parent, Wt::WFlags< Wt::Orientation > directions ) override;
+
 private:
   int width_;
   int height_;
@@ -28,6 +31,13 @@ void
 TopWidget::layoutSizeChanged( int w, int h ) {
   width_ = w;
   height_ = h;
+}
+
+inline
+void
+TopWidget::parentResized( Wt::WWidget * parent, Wt::WFlags< Wt::Orientation > orientation ) {
+  WContainerWidget::parentResized( parent, orientation );
+  std::cout << "TopWidget " << "Width " << width().value() << " Height " << height().value() << std::endl;
 }
 
 
