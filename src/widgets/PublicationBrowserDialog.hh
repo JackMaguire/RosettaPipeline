cc8f1578625b540c165456bed898ae187a90e715
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-06 22:08:34 -0500

    PublicationBrowserDialog

---
+++ src/widgets/PublicationBrowserDialog.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Wt/WContainerWidget.h>
+//#include <graph/Graph.fwd.hh>
+
+namespace widgets {
+
+class PublicationBrowserDialog : public Wt::WContainerWidget
+{
+public:
+  PublicationBrowserDialog();
+
+  virtual ~PublicationBrowserDialog();
+
+};
+
+}//namespace widgets
