8eac04aa45d5cc6d033ae05d0cdbfbd33ae8f27b
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-07 16:30:05 -0500

    adding search bar to browser

--- src/widgets/PublicationBrowserDialog.cc
+++ src/widgets/PublicationBrowserDialog.cc
@@ -4,23 +4,64 @@
 #include <widgets/GraphWidget.hh>
 #include <widgets/GraphToolbarWidget.hh>
 
+#include <publishing/load.hh>
+
 #include <Wt/WLength.h>
-//#include <Wt/WImage.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WLineEdit.h>
 #include <Wt/WGlobal.h>
-//#include <Wt/WHBoxLayout.h>
-//#include <Wt/WVBoxLayout.h>
-//#include <Wt/WBorderLayout.h>
+#include <Wt/WComboBox.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WBorderLayout.h>
 #include <Wt/WText.h>
 #include <Wt/WTable.h>
 
 namespace widgets {
 
+namespace {
+
+class SearchBarWidget : public Wt::WContainerWidget(){
+public:  
+  SearchBarWidget(){
+
+    all_publications_ = publishing::load( "/published_pipelines" );
+
+    Wt::WBorderLayout * const layout =
+      setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+
+    Wt::WComboBox * const combo_box =
+      layout->addWidget( Wt::cpp14::make_unique< Wt::WComboBox >(), Wt::LayoutPosition::West );
+    combo_box->addItem( "Title" );
+    combo_box->addItem( "Author" );
+    combo_box->addItem( "Tag" );
+    combo_box->addItem( "Key" );
+
+    Wt::WLineEdit * const search_field =
+      layout->addWidget( Wt::cpp14::make_unique< Wt::WLineEdit >(), Wt::LayoutPosition::Center );
+    search_field->setPlaceholderText( "Search" );
+
+    Wt::WPushButton * const search_button =
+      layout->addWidget( Wt::cpp14::make_unique< Wt::WPushButton >(), Wt::LayoutPosition::East );
+  }
+
+  ~SearchBarWidget() = default;
+
+private:
+  std::vector< publishing::Publication > all_publications_;
+};
+
+}
+
 PublicationBrowserDialog::PublicationBrowserDialog( ) :
   WDialog( "Published Workflows" )
 {
-  auto my_contents = contents();
+  Wt::WContainerWidget * const my_contents = contents();
+  Wt::WBorderLayout * const layout =
+    my_contents->setLayout( Wt::cpp14::make_unique< Wt::WBorderLayout >() );
+
   Wt::WTable * const table =
-    my_contents->addWidget( Wt::cpp14::make_unique< Wt::WTable >() );
+    layout->addWidget( Wt::cpp14::make_unique< Wt::WTable >(), Wt::LayoutPosition::Center );
+
   table->addStyleClass( "table form-inline" );
   table->setWidth( Wt::WLength( "100%" ) );
 
@@ -31,8 +72,9 @@ PublicationBrowserDialog::PublicationBrowserDialog( ) :
   table->elementAt( 0, 3 )->addWidget( Wt::cpp14::make_unique< Wt::WText >( "Key" ) );
   table->elementAt( 0, 4 )->addWidget( Wt::cpp14::make_unique< Wt::WText >( "" ) );
 
-
-
+  
+  SearchBarWidget * const search_bar =
+    layout->addWidget( Wt::cpp14::make_unique< SearchBarWidget >(), Wt::LayoutPosition::North );
 }
 
 PublicationBrowserDialog::~PublicationBrowserDialog(){}
