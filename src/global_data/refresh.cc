ff2243a63a4436f80a0e7fdaca0a689a10d65bc3
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-09 16:52:23 -0500

    added debug output (2)

--- src/global_data/refresh.cc
+++ src/global_data/refresh.cc
@@ -3,23 +3,28 @@
 #include <vector>
 #include <algorithm>
 
+#include <iostream>
+
 namespace global_data {
 
 thread_local std::vector< Refreshable * > objects;
 
 void refresh_all_objects() {
+  std::cout << "Refreshing " << objects.size() << " elements" << std::endl;
   for( Refreshable * obj : objects ){
     obj->refresh();
   }
 }
 
 void register_refreshable_object( Refreshable * obj ){
+  std::cout << "Adding element " << obj << std::endl;
   if( obj != 0 ){
     objects.push_back( obj );
   }
 }
 
 void deregister_refreshable_object( Refreshable * obj ){
+  std::cout << "Removing element " << obj << std::endl;
   auto iter = std::find( objects.begin(), objects.end(), obj );
   if( iter != objects.end() ){
     objects.erase( iter );
