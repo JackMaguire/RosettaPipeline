8f969f81f42faaa0e5609b9a8f91764bd01d58dd
Jack Maguire <jackmaguire1444@gmail.com>
2018-11-15 18:29:49 -0500

    added note to future self

--- src/apps/proof_of_concept/graph_view_app.cc
+++ src/apps/proof_of_concept/graph_view_app.cc
@@ -7,6 +7,8 @@
 
 #include <memory>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include <Options.hh>
 
@@ -30,11 +32,26 @@ std::unique_ptr< Wt::WApplication > createApplication( Wt::WEnvironment const & 
   app->setTitle( "Rosetta Pipeline" );
   app->root()->setStyleClass( "root" );
   app->root()->addWidget( Wt::cpp14::make_unique< widgets::TopWidget >( std::move( graph ), std::move( options ) ) );
-  app->setCssTheme("polished");
+  app->setCssTheme( "polished" );
   app->setTheme( std::make_shared< Wt::WBootstrapTheme >() ) ;
 
-  std::cout << app->url() << std::endl;
-  std::cout << app->internalSubPath( "" ) << std::endl;
+  //http://0.0.0.0:8080/?_=key/12345/ gives:
+  std::cout << app->url() << std::endl; // /?wtd=wwjZ9KkfGWFwg6T8
+  std::cout << app->internalSubPath( "" ) << std::endl; // /key/12345/
+
+  std::vector< std::string > tokens;
+  {//https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+    std::string const internal_path = app->internalSubPath( "" );
+    std::size_t prev = 0, pos = 0;
+    do {
+      pos = internal_path.find( "/", prev );
+      if ( pos == string::npos ) pos = internal_path.length();
+      std::string token = internal_path.substr(prev, pos-prev);
+      if( !token.empty() ) tokens.push_back( std::move( token ) );
+      prev = pos + 1;
+    }
+    while( prev < internal_path.length() );
+  }
 
   return app;
 }
